8cd4802eabe10c46c386b135ab48bd5bbbc85629
Gurusw <kguru8686@gmail.com>
2026-07-12 21:25:11 +0800

    changes

---
+++ src/Day1/methods.java
@@ -0,0 +1,22 @@
+package Day1;
+
+public class methods {
+	String car;
+	String color;
+	public void properties()
+	{
+		System.out.println("property");
+	}
+
+	public static void main(String[] args) 
+	{
+		
+	//	properties pp= new properties();
+		//pp.
+		
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
