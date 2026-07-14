c7f2f3aebbeee6ceea222c43837b74db047e2ca8
Gurusw <kguru8686@gmail.com>
2026-07-14 19:55:23 +0800

    switch program practice

---
+++ src/Day1/sswitch.java
@@ -0,0 +1,27 @@
+package Day1;
+
+public class sswitch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String browser="Chrome";
+    	switch(browser)
+    	{
+    	case "edge":
+    		System.out.println("THis is edge browser");
+    		break;
+    		
+    	case "chrome":
+    		System.out.println("This is Chrome browser");
+    		break;
+    	case "safari":
+    		System.out.println("This is safari browser");
+    		break;
+    		
+    		default :
+    			System.out.println("Chrome browser is the default browser");
+    			break;
+
+    	}}
+
+}
