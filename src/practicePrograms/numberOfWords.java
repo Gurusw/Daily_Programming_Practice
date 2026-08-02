37cf67cf02fd02d8948eb4309f6c39ae1623a721
Gurusw <kguru8686@gmail.com>
2026-08-02 23:54:33 +0800

    number of words

---
+++ src/practicePrograms/numberOfWords.java
@@ -0,0 +1,13 @@
+package practicePrograms;
+
+public class numberOfWords {
+
+	public static void main(String[] args)
+	{
+		String a="guru is realized";
+		String []aa=a.split(" ");
+		System.out.println(aa.length);
+
+	}
+
+}
